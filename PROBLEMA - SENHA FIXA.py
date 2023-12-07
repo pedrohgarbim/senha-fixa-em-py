a386555b52a4c48f9432fc3e69d60c61269a060c
Pedro H. Garbim <133874619+pedrohgarbim@users.noreply.github.com>
2023-12-07 01:20:25 -0300

    Add files via upload

---
+++ PROBLEMA - SENHA FIXA.py
@@ -0,0 +1,7 @@
+usuario = str(input("Usuario: "))
+senha = str(input("Senha: "))
+
+while senha != "Doutor PHG":
+    senha = str(input("SENHA INVALIDA!!! Tente novamente: "))
+
+    print("acesso permitido!")
